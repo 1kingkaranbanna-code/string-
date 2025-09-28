0dabcefe8eac36935b75c72ef64d85620c0a4208
Karan Singh <1king.karanbanna@gmail.com>
2025-09-28 10:26:52 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,15 @@
+str1="karan singh"
+print(str1.endswith("kh"))
+str1=(str1.capitalize())
+print(str1.capitalize())
+print(str1)
+print(str1.replace("a","o"))
+print(str1.replace("singh","banna"))
+print(str1.find("singh"))
+print(str1.count("a"))
+
+user = input("enter the name of user :",)
+print(len(user))
+
+str= " i have a 5$ per day , so i can earn 150$ per month"
+print(str.count("$"))
